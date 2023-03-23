805fa0dcea57c81a33dc0b236307b982a005cb88
Guocheng Zheng <121431418@qq.com>
2023-03-23 22:32:36 +0800

    updated

--- diy-part2.sh
+++ diy-part2.sh
@@ -836,7 +836,7 @@ config domain-rule
 ' >feeds/packages/net/smartdns/conf/smartdns.conf
 
 rm -f feeds/packages/net/smartdns/conf/custom.conf
-svn co https://github.com/pymumu/smartdns/branches/master/package/openwrt/custom.conf feeds/packages/net/smartdns/conf/custom.conf
+curl --retry 5 -L https://github.com/pymumu/smartdns/raw/master/package/openwrt/custom.conf -o feeds/packages/net/smartdns/conf/custom.conf
 
 latest_ver="$(curl --retry 5 https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest 2>/dev/null|grep -E 'tag_name' |grep -E 'v[0-9.]+' -o 2>/dev/null)"
 curl --retry 5 -L https://github.com/AdguardTeam/AdGuardHome/releases/download/${latest_ver}/AdGuardHome_linux_${Arch}.tar.gz | tar zxf -

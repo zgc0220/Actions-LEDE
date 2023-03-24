89b11f2afaeaec0b82b5728124ed6581ccf0fbfa
Guocheng Zheng <121431418@qq.com>
2023-03-24 13:38:43 +0800

    updated

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,20 +20,20 @@
 rm -rf package/lean/luci-app-argon-config
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
-# rm -rf package/lean/luci-app-filebrowser
-# svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-filebrowser package/lean/luci-app-filebrowser
-# sed -i "s/..\/..\/luci.mk/\$(TOPDIR)\/feeds\/luci\/luci.mk/g" package/lean/luci-app-filebrowser/Makefile
-# rm -rf package/lean/filebrowser
-# svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/filebrowser package/lean/filebrowser
-# sed -i "s/..\/..\/lang\/golang\/golang-package.mk/\$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g" package/lean/filebrowser/Makefile
+rm -rf package/lean/luci-app-filebrowser
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-filebrowser package/lean/luci-app-filebrowser
+sed -i "s/..\/..\/luci.mk/\$(TOPDIR)\/feeds\/luci\/luci.mk/g" package/lean/luci-app-filebrowser/Makefile
+rm -rf package/lean/filebrowser
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/utils/filebrowser package/lean/filebrowser
+sed -i "s/..\/..\/lang\/golang\/golang-package.mk/\$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g" package/lean/filebrowser/Makefile
 
 rm -rf package/lean/luci-app-openclash
 OPENCLASH_BRANCH=dev
 svn co https://github.com/vernesong/OpenClash/branches/$OPENCLASH_BRANCH/luci-app-openclash package/lean/luci-app-openclash
 
-# rm -rf package/lean/luci-app-smartdns
-# svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06/applications/luci-app-smartdns package/lean/luci-app-smartdns
-# sed -i "s/..\/..\/luci.mk/\$(TOPDIR)\/feeds\/luci\/luci.mk/g" package/lean/luci-app-smartdns/Makefile
+rm -rf package/lean/luci-app-smartdns
+svn co https://github.com/zgc/luci/branches/openwrt-18.06/applications/luci-app-smartdns package/lean/luci-app-smartdns
+sed -i "s/..\/..\/luci.mk/\$(TOPDIR)\/feeds\/luci\/luci.mk/g" package/lean/luci-app-smartdns/Makefile
 
 rm -rf package/lean/luci-app-adguardhome
 git clone --depth 1 -b beta https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
